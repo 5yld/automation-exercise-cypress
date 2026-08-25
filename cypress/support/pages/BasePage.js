c006af3095b9597ad30c448a066107210a9dab11
Syldson Carvalho <syldson.daniel79@gmail.com>
2026-08-25 19:55:10 -0300

    feat: adiciona Page Objects base e de login
    A classe base concentra os elementos de cabeçalho comuns a todas as
    páginas e as verificações de sessão.

---
+++ cypress/support/pages/BasePage.js
@@ -0,0 +1,55 @@
+export default class BasePage {
+  get signupLoginLink() {
+    return cy.get('a[href="/login"]');
+  }
+
+  get logoutLink() {
+    return cy.get('a[href="/logout"]');
+  }
+
+  get deleteAccountLink() {
+    return cy.get('a[href="/delete_account"]');
+  }
+
+  get cartLink() {
+    return cy.get('a[href="/view_cart"]').first();
+  }
+
+  get productsLink() {
+    return cy.get('a[href="/products"]').first();
+  }
+
+  visit(path = '/') {
+    cy.visit(path);
+    return this;
+  }
+
+  goToProducts() {
+    this.productsLink.click();
+    return this;
+  }
+
+  goToCart() {
+    this.cartLink.click();
+    return this;
+  }
+
+  logout() {
+    this.logoutLink.click();
+    return this;
+  }
+
+  /**
+   * O cabeçalho exibe "Logged in as <nome>" quando há sessão ativa. É uma
+   * verificação mais confiável que a URL, porque reflete o estado real.
+   */
+  shouldBeLoggedInAs(name) {
+    cy.contains('li', 'Logged in as').should('be.visible').and('contain', name);
+    return this;
+  }
+
+  shouldBeLoggedOut() {
+    cy.contains('a', 'Signup / Login').should('be.visible');
+    return this;
+  }
+}
---
+++ cypress/support/pages/LoginPage.js
@@ -0,0 +1,56 @@
+import BasePage from './BasePage';
+
+export default class LoginPage extends BasePage {
+  get emailField() {
+    return cy.get('[data-qa="login-email"]');
+  }
+
+  get passwordField() {
+    return cy.get('[data-qa="login-password"]');
+  }
+
+  get loginButton() {
+    return cy.get('[data-qa="login-button"]');
+  }
+
+  get signupNameField() {
+    return cy.get('[data-qa="signup-name"]');
+  }
+
+  get signupEmailField() {
+    return cy.get('[data-qa="signup-email"]');
+  }
+
+  get signupButton() {
+    return cy.get('[data-qa="signup-button"]');
+  }
+
+  get errorMessage() {
+    return cy.get('form p[style*="color"]');
+  }
+
+  open() {
+    this.visit('/login');
+    cy.contains('h2', 'Login to your account').should('be.visible');
+    return this;
+  }
+
+  login(email, password) {
+    this.emailField.clear().type(email);
+    this.passwordField.clear().type(password, { log: false });
+    this.loginButton.click();
+    return this;
+  }
+
+  startSignup(name, email) {
+    this.signupNameField.clear().type(name);
+    this.signupEmailField.clear().type(email);
+    this.signupButton.click();
+    return this;
+  }
+
+  shouldShowError(message) {
+    this.errorMessage.should('be.visible').and('have.text', message);
+    return this;
+  }
+}
